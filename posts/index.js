a2fc4512586292366b59d9019907e94a89aa84a8
DangPham112000 <dangpham112000@gmail.com>
2023-05-29 08:31:57 +0700

    add mockup

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,7 +19,21 @@ app.post("/posts", (req, res) => {
 
   posts[id] = { id, title };
 
+  axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log(req.body);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
